397ce461bbdefdfc469c9381a0c6f5792c168444
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-05-25 22:36:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+let count = 1;
+
+function addItem() {
+	const li = document.createElement("li");
+	li.innerText = `List Item ${count++}`;
+	list.appendChild(li);
+}
+
+// Initial 10 items
+for (let i = 0; i < 10; i++) {
+	addItem();
+}
+
+// Infinite scroll
+window.addEventListener("scroll", () => {
+
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 5) {
+
+		addItem();
+		addItem();
+	}
+});
